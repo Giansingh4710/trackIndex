d1e0bfcb11591e30d53b8909275f4b5b79b2f6bc
giansingh4710 <giansingh4710@gmail.com>
2023-03-20 19:43:03 +0000

    all timings uniform. changed permissions to not x

--- addData.php
+++ addData.php
@@ -8,38 +8,40 @@ error_reporting(E_ALL);
 //var_dump($_POST);
 //echo "</pre>";
 
-$timestamp = "";
-if(!empty($_POST["hours"])){
-  $timestamp = $_POST["hours"];
-}else{
-  $timestamp = "00";
+function addToCsvFromPostRequest(){
+  $timestamp = "";
+  if(!empty($_POST["hours"])){
+    $timestamp = str_pad($_POST["hours"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp = "00";
+  }
+
+  $timestamp .= ":";
+  if(!empty($_POST["mins"])){
+    $timestamp .= str_pad($_POST["mins"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp .= "00";
+  }
+
+  $timestamp .= ":";
+  if(!empty($_POST["secs"])){
+    $timestamp .= str_pad($_POST["secs"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp .= "00";
+  }
+
+  $line = array(time(), $_POST["keertani"], $_POST["description"], $timestamp, $_POST["link"], $_POST["shabadId"]);
+
+  echo "<pre>";
+  var_dump($line);
+  echo "</pre>";
+
+  $fp = fopen("data.csv", "a");
+  fputcsv($fp, $line); # $line is an array of strings (array|string[])
+  fclose($fp);
+  echo "The Data has been added to the database";
 }
 
-$timestamp .= ":";
-if(!empty($_POST["mins"])){
-  $timestamp .= $_POST["mins"];
-}else{
-  $timestamp .= "00";
-}
-
-$timestamp .= ":";
-if(!empty($_POST["secs"])){
-  $timestamp .= $_POST["secs"];
-}else{
-  $timestamp .= "00";
-}
-
-$line = array(time(), $_POST["keertani"], $_POST["description"], $timestamp, $_POST["link"], $_POST["shabadId"]);
-
-echo "<pre>";
-var_dump($line);
-echo "</pre>";
-
-$fp = fopen("data.csv", "a");
-fputcsv($fp, $line); # $line is an array of strings (array|string[])
-fclose($fp);
-echo "The Data has been added to the database";
-
-header("Location: http://santjikhata.us");
-
+addToCsvFromPostRequest()
+//header("Location: http://santjikhata.us");
 ?>
